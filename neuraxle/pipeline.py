a53f981ed7f6c919d888ab06f62416a1af487b67
alexbrillant <abrillant23@gmail.com>
2019-08-19 21:12:36 -0400

    Remove Dead Code And Set Checkpoint Pipeline Runner As Default Runner

--- neuraxle/pipeline.py
+++ neuraxle/pipeline.py
@@ -24,33 +24,7 @@ from copy import copy
 from typing import Any
 
 from neuraxle.base import BaseStep, BasePipelineRunner, TruncableSteps, BaseBarrier, NamedTupleList
-
-
-class BlockPipelineRunner(BasePipelineRunner):
-
-    def fit_transform(self, data_inputs, expected_outputs=None) -> ('BlockPipelineRunner', Any):
-        new_steps_as_tuple: NamedTupleList = []
-        for step_name, step in self.steps_as_tuple:
-            step, data_inputs = step.fit_transform(data_inputs, expected_outputs)
-            new_steps_as_tuple.append((step_name, step))
-        self.steps_as_tuple = new_steps_as_tuple
-        processed_outputs = data_inputs
-        return self, processed_outputs
-
-    def fit(self, data_inputs, expected_outputs=None) -> 'BlockPipelineRunner':
-        # TODO: don't do last transform.
-        new_steps_as_tuple: NamedTupleList = []
-        for step_name, step in self.steps_as_tuple:
-            step, data_inputs = step.fit_transform(data_inputs, expected_outputs)
-            new_steps_as_tuple.append((step_name, step))
-        self.steps_as_tuple = new_steps_as_tuple
-        return self
-
-    def transform(self, data_inputs):
-        for step_name, step in self.steps_as_tuple:
-            data_inputs = step.transform(data_inputs)
-        processed_outputs = data_inputs
-        return processed_outputs
+from neuraxle.runners import CheckpointPipelineRunner
 
 
 class DataObject:
@@ -61,58 +35,11 @@ class DataObject:
         def __hash__(self):
             return hash((self.i, self.x))
 
-
-class ResumablePipelineRunner(BasePipelineRunner):
-    # TODO: this is code to do or to delete.
-
-    def fit_transform(self, data_inputs, expected_outputs=None) -> ('ResumablePipelineRunner', Any):
-        for step_name, step in self.steps_as_tuple[:-1]:
-            # TODO: overwrite step's self return value on fitting.
-            data_inputs = step.fit_transform(data_inputs, expected_outputs)
-        processed_outputs = self.steps_as_tuple[-1][-1].fit_transform(data_inputs, expected_outputs)
-        return processed_outputs
-
-    def fit(self, data_inputs, expected_outputs=None) -> 'ResumablePipelineRunner':
-        for step_name, step in self.steps_as_tuple[:-1]:
-            # TODO: overwrite step's self return value on fitting.
-            data_inputs = step.fit_transform(data_inputs, expected_outputs)
-        processed_outputs = self.steps_as_tuple[-1][-1].fit(data_inputs, expected_outputs)
-        return processed_outputs
-
-    def transform(self, data_inputs):
-
-        dos = [DataObject(i, di) for i, di in enumerate(data_inputs)]
-
-        # TODO: review this and do it in fit also.
-        steps = self.resume_transform_entry_point(dos, self.steps_as_tuple)
-
-        for step_name, step in steps:
-            data_inputs = step.transform(data_inputs)
-        processed_outputs = data_inputs
-        return processed_outputs
-
-    def resume_transform_entry_point(self, dos, steps_as_tuple):
-        steps = copy(steps_as_tuple)
-        for step_name, step in reversed(steps_as_tuple):
-
-            # TODO: maybe change to: if hasattr(step, 'should_resume'):
-            if isinstance(step, BaseBarrier) or isinstance(step, Pipeline):
-
-                if step.should_resume(dos):
-                    if isinstance(step, Pipeline):  # TODO: maybe change to: if hasattr(step, 'trim_for_resume'):
-                        step = step.trim_for_resume(dos)
-                        steps[step_name] = step
-                    steps = steps_as_tuple[step_name:]  # TODO: fix get
-                    return steps
-
-
 class Pipeline(TruncableSteps):
 
-    def __init__(
-            self,
-            steps: NamedTupleList,
-            pipeline_runner: BasePipelineRunner = BlockPipelineRunner(),
-    ):
+    def __init__(self,
+                 steps: NamedTupleList,
+                 pipeline_runner: BasePipelineRunner = CheckpointPipelineRunner()):
         BaseStep.__init__(self)
         TruncableSteps.__init__(self, steps)
         self.pipeline_runner: BasePipelineRunner = pipeline_runner
