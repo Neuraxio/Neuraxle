4367b305dd97d6ba73ab2105d30e2322f3c7f086
alexbrillant <abrillant23@gmail.com>
2019-09-30 18:36:43 -0400

    MiniBatchSequentialPipeline unit tests WIP

--- testing/test_minibatch_sequential_pipeline.py
+++ testing/test_minibatch_sequential_pipeline.py
@@ -2,7 +2,7 @@ from neuraxle.pipeline import MiniBatchSequentialPipeline, Barrier
 from testing.test_pipeline import SomeStep
 
 
-def test_streaming_pipeline_should_transform_steps_sequentially_for_each_batch():
+def test_mini_batch_sequential_pipeline_should_transform_steps_sequentially_for_each_barrier_for_each_batch():
     p = MiniBatchSequentialPipeline([
         SomeStep(),
         SomeStep(),
@@ -12,4 +12,38 @@ def test_streaming_pipeline_should_transform_steps_sequentially_for_each_batch()
         Barrier()
     ], batch_size=10)
 
-    p.transform()
+    outputs = p.transform(range(100))
+
+    # assert steps have received the right batches
+    # assert steps have transformed the data correctly
+
+
+def test_mini_batch_sequential_pipeline_should_fit_transform_steps_sequentially_for_each_barrier_for_each_batch():
+    p = MiniBatchSequentialPipeline([
+        SomeStep(),
+        SomeStep(),
+        Barrier(),
+        SomeStep(),
+        SomeStep(),
+        Barrier()
+    ], batch_size=10)
+
+    outputs = p.transform(range(100))
+
+    # assert steps have received the right batches
+    # assert steps have transformed the data correctly
+    # assert steps have been fitted with the right batches in the right order
+
+
+def test_mini_batch_sequential_pipeline_joiner():
+    p = MiniBatchSequentialPipeline([
+        SomeStep(),
+        SomeStep(),
+        Barrier(),
+        SomeStep(),
+        SomeStep(),
+        Barrier()
+    ], batch_size=10)
+
+    # TODO: joiner ????????
+
