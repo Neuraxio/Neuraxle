89ff760627f90b67427242827bd4a746ec2096d0
alexbrillant <abrillant23@gmail.com>
2019-10-22 15:25:52 -0400

    Fix Test AutoML Checkpoint

--- testing/examples/test_automl_checkpoint.py
+++ testing/examples/test_automl_checkpoint.py
@@ -1,7 +1,4 @@
-import numpy as np
-
-from examples.auto_ml_checkpoint import run_random_search_normal_pipeline, run_random_search_resumable_pipeline, main
-from neuraxle.hyperparams.space import HyperparameterSamples
+from examples.auto_ml_checkpoint import main
 
 
 def test_automl_checkpoints():
