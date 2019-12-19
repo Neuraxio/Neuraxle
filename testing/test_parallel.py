72f339b6f3c8fefd08477103818c5806ca79697f
alexbrillant <abrillant23@gmail.com>
2019-12-19 15:15:43 -0500

    Implement Saver Parallel Transform, And Full Dump Loading By Path

--- testing/test_parallel.py
+++ testing/test_parallel.py
@@ -11,18 +11,23 @@ from neuraxle.pipeline import Pipeline
 from neuraxle.steps.sklearn import SKLearnWrapper
 
 
-def test_saver_parallel_transform_should_parallelize():
+def test_saver_parallel_transform(tmpdir):
     boston = load_boston()
     X, y = shuffle(boston.data, boston.target, random_state=13)
     X = X.astype(np.float32)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, shuffle=False)
+    sk_learn_pca = SKLearnWrapper(PCA(n_components=2), HyperparameterSpace({"n_components": RandInt(1, 3)}))
+    sk_learn_pca = sk_learn_pca.fit(X_train, y_train)
+    expected_outputs = sk_learn_pca.transform(X_train)
 
     p = Pipeline([
         SaverParallelTransform(
-            SKLearnWrapper(PCA(n_components=2), HyperparameterSpace({"n_components": RandInt(1, 3)})),
+            sk_learn_pca,
             n_jobs=10,
             batch_size=10
         )
-    ])
+    ], cache_folder=tmpdir)
 
-    outputs_train = p.transform(X_train)
+    actual_outputs = p.transform(X_train)
+
+    assert np.array_equal(expected_outputs, actual_outputs)
