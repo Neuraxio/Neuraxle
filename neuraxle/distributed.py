51072a028218d18d1c3896b7937555be090a943d
alexbrillant <abrillant23@gmail.com>
2020-01-02 18:44:48 -0500

    Implement RestApiScheduler For ClusteringWrapper

--- neuraxle/distributed.py
+++ neuraxle/distributed.py
@@ -21,16 +21,33 @@ Neuraxle Steps for distributed computing in the cloud
 
 """
 import math
+import os
 from abc import ABC, abstractmethod
 from queue import Queue
 from typing import List, Iterable
 
+import numpy as np
+import requests
+from flask import jsonify
+from werkzeug.utils import secure_filename
+
+from neuraxle.api.flask import RestAPICaller
 from neuraxle.base import MetaStepMixin, BaseStep, NonFittableMixin, ExecutionContext
 from neuraxle.data_container import DataContainer
 from neuraxle.steps.numpy import NumpyConcatenateInnerFeatures
 
 
 class BaseClusteringScheduler(ABC):
+    """
+    Base class for a clustering scheduler that distributes task to multiple workers.
+    Used by the :class:`ClusteringWrapper` for distributed processing.
+
+    .. seealso::
+        :class:`RestApiScheduler`,
+        :class:`ClusteringWrapper`,
+        :class:`DataContainer`,
+        :class:`ExecutionContext`
+    """
     def __init__(self):
         self.started = False
 
@@ -39,11 +56,7 @@ class BaseClusteringScheduler(ABC):
         pass
 
     @abstractmethod
-    def dispatch(self, batches: Iterable[DataContainer]):
-        pass
-
-    @abstractmethod
-    def get_results(self) -> DataContainer:
+    def dispatch(self, batches: Iterable[DataContainer], context: ExecutionContext) -> List[DataContainer]:
         pass
 
     @abstractmethod
@@ -51,64 +64,67 @@ class BaseClusteringScheduler(ABC):
         pass
 
 
-class LocalScheduler(BaseClusteringScheduler):
-    def dispatch(self, batches: Iterable[DataContainer]):
-        workers = Queue()
+class RestApiScheduler(BaseClusteringScheduler):
+    """
+    Clustering scheduler that calls rest api endpoints for distributed processing.
 
-        self.results = []
+    Please refer to the full example in :class:`ClusteringWrapper` for further details.
 
-        for batch in batches:
-            worker = workers.get()
-            worker.transform(batch)
-            workers.put(worker)
-
-    def get_results(self) -> List[DataContainer]:
-        return self.results
-
-    def kill(self, context: ExecutionContext):
-        pass
-
-    def spawn(self, context: ExecutionContext):
-        self.started = True
-
-
-class LocalWorker:
-    def start(self):
-        # start api that saves a pipeline dump ?
-        app = FlaskRestApiWrapper(
-            json_decoder=CustomJSONDecoderFor2DArray(),
-            wrapped=Pipeline(...),
-            json_encoder=CustomJSONEncoderOfOutputs(),
-        ).get_app()
-
-        # start api/socket that transforms the data container ?
-        app = FlaskRestApiWrapper(
-            json_decoder=CustomJSONDecoderFor2DArray(),
-            wrapped=Pipeline(...),
-            json_encoder=CustomJSONEncoderOfOutputs(),
-        ).get_app()
-        # add a socket send step at the end of the pipeline
-
-        app.run(debug=False, port=5000)
-
-
-class ExternalScheduler(BaseClusteringScheduler):
+    .. seealso::
+        :class:`BaseClusteringScheduler`,
+        :class:`ClusteringWrapper`,
+        :class:`ExecutionContext`,
+        :class:`RestApiCaller`,
+    """
     def __init__(self, hosts: List[str]):
         super().__init__()
         self.hosts = hosts
+        self.workers = Queue()
 
-    def spawn(self, context: ExecutionContext):
-        self.started = True
-        pass
+    def dispatch(self, batches: Iterable[DataContainer], context: ExecutionContext) -> List[DataContainer]:
+        """
+        Dispatch tasks to the workers using rest api calls.
 
-    def dispatch(self, batches: Iterable[DataContainer]):
-        pass
+        :param batches: an iterable list of data containers
+        :param context: execution context
+        :type context: ExecutionContext
+        :return: list of data container
+        :rtype: List[DataContainer]
+        """
+        results = []
 
-    def get_results(self) -> DataContainer:
-        pass
+        for batch in batches:
+            worker = self.workers.get()
+            results.append(worker.handle_transform(batch, context))
+            self.workers.put(worker)
+
+        return results
 
     def kill(self, context: ExecutionContext):
+        # TODO: what is there to kill here ?
         pass
+
+    def spawn(self, context: ExecutionContext):
+        """
+        Spawn a worker for each host.
+
+        1) Send the full pipeline dump files to the workers using the rest api endpoints in self.hosts
+        2) Add an :class:`ApiCaller` step in the queue of workers for each hosts.
+
+        :param context: execution context
+        :return:
+        """
+        self.started = True
+        for host in self.hosts:
+            saved_step_files = context.get_all_saved_step_files()
+            files = {path.replace(str(context.root) + '/', ''): open(file_path, 'rb') for path, file_path in saved_step_files}
+            r = requests.post(host, files=files)
+
+            for f in files.values():
+                f.close()
+
+            if r.status_code == 201:
+                self.workers.put(RestAPICaller(host))
 
 
 class ClusteringWrapper(MetaStepMixin, BaseStep):
@@ -122,10 +138,10 @@ class ClusteringWrapper(MetaStepMixin, BaseStep):
         p = Pipeline([
             ClusteringWrapper(
                 Pipeline([..]),
-                scheduler=LocalScheduler(),
-                joiner=NumpyConcatenateInnerFeatures(),
+                scheduler=RestApiScheduler(['http://127.0.0.1:5000/pipeline']),
+                joiner=NumpyConcatenateOuterBatch(),
                 n_jobs=10,
-                batch_size=100
+                batch_size=10
             )
         ])
 
@@ -170,22 +186,20 @@ class ClusteringWrapper(MetaStepMixin, BaseStep):
         :param context:
         :return:
         """
-        context.save_last()
+        wrapped_context = context.push(self.wrapped)
+        wrapped_context = wrapped_context.push(wrapped_context.save_last())
 
-        self.scheduler.spawn(context)
+        self.scheduler.spawn(wrapped_context)
 
         # create batches to transform
         batch_size = self._get_batch_size(data_container)
         data_container_batches = data_container.convolved_1d(stride=batch_size, kernel_size=batch_size)
 
         # dispatch batches to transform
-        self.scheduler.dispatch(data_container_batches)
-
-        # wait for the tasks to complete and retrieve the results.
-        data_containers = self.scheduler.get_results()
+        data_containers = self.scheduler.dispatch(data_container_batches, wrapped_context)
 
         # kill all the workers
-        self.scheduler.kill(context)
+        self.scheduler.kill(wrapped_context)
 
         # return the resulting data containers for each task
         return DataContainer(
@@ -219,3 +233,124 @@ class ClusteringWrapper(MetaStepMixin, BaseStep):
         else:
             batch_size = self.batch_size
         return batch_size
+
+
+class RestWorker:
+    """
+    Worker class for the :class:`RestApiScheduler`.
+
+
+    Easily start a flask server for a rest api worker :
+
+    .. code-block:: python
+
+        worker = RestWorker(host='127.0.0.1', port=5000)
+        worker.start(upload_folder='/home/neuraxle/Documents/cluster')
+
+
+    .. seealso::
+        :class:`BaseClusteringScheduler`,
+        :class:`LocalScheduler`,
+        :class:`ExternalScheduler`,
+        :class:`MetaStepMixin`,
+        :class:`BaseStep`,
+        :class:`DataContainer`
+    """
+    def __init__(self, ressource_name='pipeline', host: str = None, port: int = 5000):
+        self.ressource_name = ressource_name
+        self.host = host
+        self.port = port
+
+    def get_app(self):
+        """
+        :return: a Flask app (as given by `app = Flask(__name__)` and then configured).
+        """
+        from flask import Flask, request
+        from flask_restful import Api, Resource
+
+        app = Flask(__name__)
+        api = Api(app)
+
+        class PipelineRessource(Resource):
+            def post(self):
+                """
+                Post route to save the full pipeline dump locally.
+                Receives the full dump for each pipeline steps inside the files attribute.
+
+                :return: response
+                """
+                if len(request.files) == 0:
+                    response = jsonify({'message': 'No file part in the request'})
+                    response.status_code = 400
+                    return response
+
+                for file in request.files:
+                    filename = secure_filename(request.files[file].filename)
+                    upload_path = os.path.join(app.config['UPLOAD_FOLDER'], file.replace(filename, ''))
+
+                    if not os.path.exists(upload_path):
+                        os.makedirs(upload_path)
+
+                    file_path = os.path.join(upload_path, filename)
+                    request.files[file].save(file_path)
+
+                response = jsonify({'message': 'Files successfully uploaded'})
+                response.status_code = 201
+
+                return response
+
+            def get(self):
+                """
+                Get route to transform the data container received in the body :
+
+                .. code-block:: python
+
+                    {
+                        current_ids: [...],
+                        data_inputs: [...],
+                        expected_outputs: [...],
+                        summary_id: ...
+                    }
+
+                :return: transformed data container
+                """
+                context = ExecutionContext(app.config['UPLOAD_FOLDER'])
+                body = request.get_json()
+                pipeline = context.load(body['path'])
+
+                data_container = pipeline.handle_transform(
+                    DataContainer(
+                        current_ids=body['current_ids'],
+                        data_inputs=body['data_inputs'],
+                        expected_outputs=body['expected_outputs'],
+                        summary_id=body['summary_id'],
+                    ),
+                    context
+                )
+
+                response = jsonify({
+                    'current_ids': np.array(data_container.current_ids).tolist(),
+                    'data_inputs': np.array(data_container.data_inputs).tolist(),
+                    'expected_outputs': np.array(data_container.expected_outputs).tolist(),
+                    'summary_id': data_container.summary_id
+                })
+                response.status_code = 200
+
+                return response
+
+        api.add_resource(PipelineRessource, '/' + self.ressource_name)
+
+        return app
+
+    def start(self, upload_folder):
+        """
+        Start the worker with the given upload folder.
+
+        :param upload_folder: upload folder
+        :type upload_folder: str
+        :return:
+        """
+        app = self.get_app()
+        app.config['UPLOAD_FOLDER'] = upload_folder
+        app.run(host=self.host, debug=False, port=self.port)
+
