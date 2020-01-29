Fix numpy array broadcasting, and zipping to concatenate 1d to 3d, 2d to 3d, and 1d to 2d

neuraxle/steps/data.py
@@ -235,10 +235,24 @@ class ZipData(NonFittableMixin, NonTransformableMixin, BaseStep):
                 sub_data_containers_to_zip.append(sub_data_container)
 
         for data_container_to_zip in sub_data_containers_to_zip:
-            data_inputs = np.dstack((data_container.data_inputs, data_container_to_zip.data_inputs))
-            data_container.set_data_inputs(data_inputs)
-
-            expected_outputs = np.dstack((data_container.expected_outputs, data_container_to_zip.expected_outputs))
-            data_container.set_expected_outputs(expected_outputs)
+            data_container = self._zip_data_container(data_container, data_container_to_zip)
 
         return data_container
+
+    def _zip_data_container(self, data_container, data_container_to_zip):
+        data_inputs = self._zip_np_arrays(data_container.data_inputs, data_container_to_zip.data_inputs)
+        data_container.set_data_inputs(data_inputs)
+
+        expected_outputs = self._zip_np_arrays(data_container.expected_outputs, data_container_to_zip.expected_outputs)
+        data_container.set_expected_outputs(expected_outputs)
+
+        return data_container
+
+    def _zip_np_arrays(self, np_array, np_array_to_zip):
+        while len(np_array_to_zip.shape) < len(np_array.shape):
+            np_array_to_zip = np.expand_dims(np_array_to_zip, axis=-1)
+
+        target_shape = tuple(list(np_array.shape[:-1]) + [np_array_to_zip.shape[-1]])
+        np_array_to_zip = np.broadcast_to(np_array_to_zip, target_shape)
+
+        return np.concatenate((np_array, np_array_to_zip), axis=-1)

testing/steps/test_zip_data.py
@@ -53,10 +53,14 @@ def test_zip_data_should_merge_1d_with_3d():
     data_container = p.handle_transform(data_container, ExecutionContext())
 
     # Then
+    broadcasted_data_inputs_1d = np.broadcast_to(np.expand_dims(data_container_1d.data_inputs, axis=-1), shape=(SHAPE_3D[0], SHAPE_3D[1]))
+    broadcasted_expected_outputs_1d = np.broadcast_to(np.expand_dims(data_container_1d.expected_outputs, axis=-1), shape=(SHAPE_3D[0], SHAPE_3D[1]))
+
+    assert np.array_equal(data_container.data_inputs[..., -1], broadcasted_data_inputs_1d)
+    assert np.array_equal(data_container.expected_outputs[..., -1], broadcasted_expected_outputs_1d)
+
     assert data_container.data_inputs.shape == (SHAPE_3D[0], SHAPE_3D[1], SHAPE_3D[2] + 1)
     assert data_container.expected_outputs.shape == (SHAPE_3D[0], SHAPE_3D[1], SHAPE_3D[2] + 1)
-    assert np.array_equal(data_container.data_inputs[..., -1], data_container_1d.data_inputs)
-    assert np.array_equal(data_container.expected_outputs[..., -1], data_container_1d.expected_outputs)
 
 
 def test_zip_data_should_merge_1d_with_2d():
@@ -77,8 +81,8 @@ def test_zip_data_should_merge_1d_with_2d():
     # Then
     assert data_container.data_inputs.shape == (SHAPE_2D[0], SHAPE_2D[1] + 1)
     assert data_container.expected_outputs.shape == (SHAPE_2D[0], SHAPE_2D[1] + 1)
-    assert np.array_equal(data_container.data_inputs[..., -1], data_container_1d.data_inputs * SHAPE_2D[1])
-    assert np.array_equal(data_container.expected_outputs[..., -1], data_container_1d.expected_outputs * SHAPE_2D[1])
+    assert np.array_equal(data_container.data_inputs[..., -1], data_container_1d.data_inputs)
+    assert np.array_equal(data_container.expected_outputs[..., -1], data_container_1d.expected_outputs)
 
 
 def _create_data_source(shape):