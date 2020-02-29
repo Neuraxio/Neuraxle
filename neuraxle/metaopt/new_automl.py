1afa3859bab95047f5cb093b9d4e4fb08d2bd97a
alexbrillant <abrillant23@gmail.com>
2020-02-29 15:28:38 -0500

    Fix Metrics In Trainer

--- neuraxle/metaopt/new_automl.py
+++ neuraxle/metaopt/new_automl.py
@@ -99,14 +99,29 @@ class InMemoryHyperparamsRepository(HyperparamsRepository):
 
     def save_model(self, step: BaseStep):
         trial_hash = self._get_trial_hash(step.get_hyperparams().to_flat_as_dict_primitive())
-        step.set_name(trial_hash).save(full_dump=True)
+        step.set_name(trial_hash).save(ExecutionContext(self.cache_folder), full_dump=True)
         return step
 
 
 class Trial:
     def __init__(self, hyperparams):
         self.status = None
+        self.train_score = None
+        self.validation_score = None
+        self.error = None
+        self.error_traceback = None
+        self.metrics_results_train = None
+        self.metrics_results_validation = None
+        self.model = None
+
         self.hyperparams = hyperparams
+
+    def update_trial(self, train_score, validation_score, metrics_results_train, metrics_results_validation, model):
+        self.train_score = train_score
+        self.validation_score = validation_score
+        self.metrics_results_train = metrics_results_train
+        self.metrics_results_validation = metrics_results_validation
+        self.model = model
 
     def set_failed_trial(self, error):
         self.status = TRIAL_STATUS.FAILED
@@ -121,23 +136,25 @@ class Trial:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.end_time = datetime.datetime.now()
         self.status = TRIAL_STATUS.SUCCESS
-        self.status = TRIAL_STATUS.FAILED
         return self
 
 
 class Trainer:
-    def __init__(self, p, score, metrics, callbacks, trial_repository):
+    def __init__(self, p, score, metrics, callbacks, trial_repository, print_metrics=True, print_func=None):
         self.p = p
-
         self.score = score
 
         self.metrics = metrics
+        self._initialize_metrics(metrics)
+
         self.callbacks = callbacks
 
-        self.metrics_results_train = None
-        self.metrics_results_validation = None
-
         self.trial_repository = trial_repository
+        if print_func is None:
+            print_func = print
+
+        self.print_func = print_func
+        self.print_metrics = print_metrics
 
     def train(self, train_data_container, validation_data_container, epochs, context: ExecutionContext):
         early_stopping = False
@@ -148,25 +165,57 @@ class Trainer:
             y_pred_train = self.p.handle_predict(train_data_container, context)
             y_pred_val = self.p.handle_predict(validation_data_container, context)
 
-            self.metrics_results_train = calculate_metrics_results(y_pred_train, self.metrics)
-            self.metrics_results_validation = calculate_metrics_results(y_pred_val, self.metrics)
+            self._update_metrics_results(y_pred_train, train_metrics=True)
+            self._update_metrics_results(y_pred_val, train_metrics=False)
 
-            train_score = self.score(y_pred_train)
-            validation_score = self.score(y_pred_val)
+            train_score = self.score(y_pred_train.data_inputs, y_pred_train.expected_outputs)
+            validation_score = self.score(y_pred_val.data_inputs, y_pred_val.expected_outputs)
+
+            self.trial_repository.update_trial(
+                train_score=train_score,
+                validation_score=validation_score,
+                metrics_results_train=self.metrics_results_train,
+                metrics_results_validation=self.metrics_results_validation,
+                model=self.p
+            )
 
             for callback in self.callbacks:
                 if callback.call(y_pred_train, y_pred_val, train_score, validation_score):
-                    self.trial_repository.update_trial(
-                        training_score=train_score,
-                        validation_score=validation_score,
-                        model_to_checkpoint_if_better_score=self.p
-                    )
                     early_stopping = True
 
             if early_stopping:
                 break
 
         return self, train_data_container, validation_data_container
+
+    def _initialize_metrics(self, metrics):
+        """
+        Initialize metrics results dict for train, and validation using the metrics function dict.
+
+        :param metrics: metrics function dict
+        :type metrics: dict
+        :return:
+        """
+        self.metrics_results_train = {}
+        self.metrics_results_validation = {}
+
+        for m in metrics:
+            self.metrics_results_train[m] = []
+            self.metrics_results_validation[m] = []
+
+    def _update_metrics_results(self, data_container, train_metrics):
+        result = {}
+        for metric_name, metric_function in self.metrics.items():
+            result_metric = metric_function(data_container.data_inputs, data_container.expected_outputs)
+            result[metric_name] = result_metric
+
+            if train_metrics:
+                self.metrics_results_train[metric_name].append(result_metric)
+            else:
+                self.metrics_results_validation[metric_name].append(result_metric)
+
+        if self.print_metrics:
+            self.print_func(result)
 
 
 class AutoML(ForceHandleOnlyMixin, BaseStep):
@@ -181,7 +230,7 @@ class AutoML(ForceHandleOnlyMixin, BaseStep):
             epochs: int,
             callbacks: List[Callable],
             higher_score_is_better=False,
-            print_func: Callable=None,
+            print_func: Callable = None,
             only_save_new_best_model=True
     ):
         BaseStep.__init__(self)
@@ -205,15 +254,15 @@ class AutoML(ForceHandleOnlyMixin, BaseStep):
         self.metrics = metrics
 
     def _fit_data_container(self, data_container: DataContainer, context: ExecutionContext) -> 'BaseStep':
-        training_data_container, validation_data_container = self.validation_technique.split_data_container(data_container)
+        training_data_container, validation_data_container = self.validation_technique.split_data_container(
+            data_container)
         best_score = None
 
         for trial_number in range(self.n_trial):
             auto_ml_data = self._load_auto_ml_data(trial_number)
             p = self.validation_technique.set_step(copy.copy(self.pipeline))
-
-            try:
-                with self.hyperparams_repository.new_experiment(auto_ml_data) as repo_trial:
+            with self.hyperparams_repository.new_experiment(auto_ml_data) as repo_trial:
+                try:
                     p.update_hyperparams(repo_trial.hyperparams)
 
                     p, training_data_container, validation_data_container = Trainer(
@@ -228,44 +277,42 @@ class AutoML(ForceHandleOnlyMixin, BaseStep):
                         epochs=self.epochs,
                         context=context
                     )
-
-                    train_score = self.scoring_function(training_data_container.train_data_container)
-                    validation_score = self.scoring_function(training_data_container.validation_data_container)
-
-                    repo_trial.set_score(train_score, validation_score)
-            except Exception as error:
-                track = traceback.format_exc()
-                self.print_func(track)
-                repo_trial.set_failed(error)
+                except Exception as error:
+                    track = traceback.format_exc()
+                    self.print_func(track)
+                    repo_trial.set_failed(error)
 
             is_new_best_score = self._get_best_score(best_score, repo_trial)
+            if is_new_best_score:
+                best_score = repo_trial.validation_score
 
             self.hyperparams_repository.save_trial(repo_trial)
-            self._save_model_if_needed(is_new_best_score, p)
+            self._save_model_if_needed(repo_trial.model, is_new_best_score)
 
         return self
 
     def _get_best_score(self, best_score, repo_trial):
+        if best_score is None:
+            return True
+
         new_best_score = False
         if repo_trial.status == TRIAL_STATUS.FAILED:
             return new_best_score
 
         if repo_trial.validation_score < best_score and not self.higher_score_is_better:
-            best_score = repo_trial.validation_score
             new_best_score = True
         if repo_trial.validation_score > best_score and self.higher_score_is_better:
-            best_score = repo_trial.validation_score
             self.print_func('new best score: {}'.format(best_score))
             self.print_func('new best hyperparams: {}'.format(repo_trial.hyperparams))
             new_best_score = True
         return new_best_score
 
-    def _save_model_if_needed(self, is_new_best_score, p):
+    def _save_model_if_needed(self, pipeline, is_new_best_score):
         if self.only_save_new_best_model:
             if is_new_best_score:
-                self.hyperparams_repository.save_model(p)
+                self.hyperparams_repository.save_model(pipeline)
         else:
-            self.hyperparams_repository.save_model(p)
+            self.hyperparams_repository.save_model(pipeline)
 
     def _load_auto_ml_data(self, trial_number: int) -> AutoMLContainer:
         """
