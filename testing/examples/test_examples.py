d46a15577f200fe0b4f52e3acb3c44b8186a0467
alexbrillant <abrillant23@gmail.com>
2020-03-26 21:57:12 -0400

    Add Gallery Folder For Examples

--- testing/examples/test_examples.py
+++ testing/examples/test_examples.py
@@ -2,22 +2,22 @@ from py._path.local import LocalPath
 
 
 def test_auto_ml_checkpoint(tmpdir: LocalPath):
-    from examples.auto_ml_checkpoint import main
+    from examples.caching.auto_ml_checkpoint import main
     main(tmpdir)
 
 
 def test_boston_housing_meta_optimization(tmpdir: LocalPath):
-    from examples.boston_housing_meta_optimization import main
+    from examples.sklearn.boston_housing_meta_optimization import main
     main(tmpdir)
 
 
 def test_boston_housing_regression_with_model_stacking():
-    from examples.boston_housing_regression_with_model_stacking import main
+    from examples.sklearn.boston_housing_regression_with_model_stacking import main
     main()
 
 
 def test_easy_rest_api_serving():
-    from examples.easy_rest_api_serving import main
+    from examples.deployment.easy_rest_api_serving import main
     main()
 
 
@@ -27,20 +27,20 @@ def test_hyperparams():
 
 
 def test_inverse_transform():
-    from examples.inverse_transform import main
+    from examples.getting_started.inverse_transform import main
     main()
 
 
 def test_nested_pipelines():
-    from examples.nested_pipelines import main
+    from examples.getting_started.nested_pipelines import main
     main()
 
 
 def test_non_fittable_mixin():
-    from examples.non_fittable_mixin import main
+    from examples.getting_started.non_fittable_mixin import main
     main()
 
 
 def test_label_encoder_across_multiple_columns():
-    from examples.label_encoder_across_multiple_columns import main
+    from examples.getting_started.label_encoder_across_multiple_columns import main
     main()
