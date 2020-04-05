309b13584cd252b930957799f556d827c9de5c8f
alexbrillant <abrillant23@gmail.com>
2020-04-05 10:57:18 -0400

    Somewhat Fix Pipeline To SKLearn Test With Set Params Recursive Method

--- neuraxle/steps/sklearn.py
+++ neuraxle/steps/sklearn.py
@@ -80,7 +80,13 @@ class SKLearnWrapper(BaseStep):
 
     def set_hyperparams(self, hyperparams: HyperparameterSamples) -> BaseStep:
         BaseStep.set_hyperparams(self, hyperparams)
-        flat_hyperparams = HyperparameterSamples(self.hyperparams).to_flat()
+
+        # this should work
+        flat_hyperparams = {
+            '__'.join(key.split('__')[1:]): value for key, value in HyperparameterSamples(hyperparams).to_flat().items()
+        }
+
+        flat_hyperparams = HyperparameterSamples(flat_hyperparams).to_flat()
         self.wrapped_sklearn_predictor.set_params(**HyperparameterSamples(flat_hyperparams).to_flat_as_dict_primitive())
         return self
 
