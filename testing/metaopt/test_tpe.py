fadcbf34ebee28632c2a961659d43b0f6b48d908
alexbrillant <abrillant23@gmail.com>
2020-09-03 13:15:00 -0400

    Implement Robust TPE Unit Tests

--- testing/metaopt/test_tpe.py
+++ testing/metaopt/test_tpe.py
@@ -1,12 +1,13 @@
 import numpy as np
 import pytest
 from joblib import Parallel, delayed
+from neuraxle.metaopt.trial import TRIAL_STATUS, Trials
 from sklearn.metrics import mean_squared_error
 
 from neuraxle.hyperparams.distributions import Uniform, LogNormal, Normal, Choice, Quantized, LogUniform
 from neuraxle.hyperparams.space import HyperparameterSpace
 from neuraxle.metaopt.auto_ml import InMemoryHyperparamsRepository, AutoML, ValidationSplitter, \
-    RandomSearchHyperparameterSelectionStrategy
+    RandomSearchHyperparameterSelectionStrategy, BaseHyperparameterSelectionStrategy
 from neuraxle.metaopt.callbacks import MetricCallback, ScoringCallback
 from neuraxle.metaopt.tpe import TreeParzenEstimatorHyperparameterSelectionStrategy
 from neuraxle.pipeline import Pipeline
@@ -25,22 +26,22 @@ import os
             'add': Choice(choice_list=[0, 1.5, 2, 3.5, 4, 5, 6]),
         }))
     ])),
-    (7.0, Pipeline([
+    (3.5, Pipeline([
         FitTransformCallbackStep().set_name('callback'),
         AddN(0.).set_hyperparams_space(HyperparameterSpace({
-            'add': Quantized(hd=Uniform(-2, 10)),
+            'add': Quantized(hd=Uniform(0, 10)),
         })),
         AddN(0.).set_hyperparams_space(HyperparameterSpace({
-            'add': Quantized(hd=Uniform(-2, 10)),
+            'add': Quantized(hd=Uniform(0, 10)),
         }))
     ])),
     (3.5, Pipeline([
         FitTransformCallbackStep().set_name('callback'),
         AddN(0.).set_hyperparams_space(HyperparameterSpace({
-            'add': LogUniform(min_included=1.0, max_included=10.),
+            'add': LogUniform(min_included=1.0, max_included=5.0),
         })),
         AddN(0.).set_hyperparams_space(HyperparameterSpace({
-            'add': LogUniform(min_included=1.0, max_included=10.),
+            'add': LogUniform(min_included=1.0, max_included=5.0),
         }))
     ])),
     (3.5, Pipeline([
@@ -55,54 +56,70 @@ import os
     (3.5, Pipeline([
         FitTransformCallbackStep().set_name('callback'),
         AddN(0.).set_hyperparams_space(HyperparameterSpace({
-            'add': Normal(mean=2.0, std=2.0, hard_clip_min=-2, hard_clip_max=6),
+            'add': Normal(mean=3.0, std=2.0, hard_clip_min=0, hard_clip_max=6),
         })),
         AddN(0.).set_hyperparams_space(HyperparameterSpace({
-            'add': Normal(mean=2.0, std=2.0, hard_clip_min=-2, hard_clip_max=6),
+            'add': Normal(mean=3.0, std=2.0, hard_clip_min=0, hard_clip_max=6),
         })),
     ])),
-    (1.5, Pipeline([
+    (3.5, Pipeline([
         FitTransformCallbackStep().set_name('callback'),
         AddN(0.).set_hyperparams_space(HyperparameterSpace({
-            'add': Uniform(-1, 3),
+            'add': Uniform(0, 6),
         })),
         AddN(0.).set_hyperparams_space(HyperparameterSpace({
-            'add': Uniform(-1, 3),
+            'add': Uniform(0, 6),
         }))
     ]))
 ])
 def test_tpe(expected_output_mult, pipeline, tmpdir):
     # Given
     tpe_scores = Parallel(n_jobs=-2)(
-        delayed(_test_tpe_score)(expected_output_mult, pipeline, os.path.join(tmpdir, 'tpe', str(i)))
-        for i in range(20)
+        delayed(_test_trial_scores)(
+            expected_output_mult,
+            pipeline,
+            TreeParzenEstimatorHyperparameterSelectionStrategy(
+                number_of_initial_random_step=10,
+                quantile_threshold=0.3,
+                number_good_trials_max_cap=25,
+                number_possible_hyperparams_candidates=100,
+                prior_weight=0.,
+                use_linear_forgetting_weights=False,
+                number_recent_trial_at_full_weights=25
+            ),
+            os.path.join(tmpdir, 'tpe', str(i))
+        ) for i in range(2)
     )
+
     random_scores = Parallel(n_jobs=-2)(
-        delayed(_test_random_score)(expected_output_mult, pipeline, os.path.join(tmpdir, 'random', str(i)))
-        for i in range(20)
+        delayed(_test_trial_scores)(
+            expected_output_mult,
+            pipeline,
+            RandomSearchHyperparameterSelectionStrategy(),
+            os.path.join(tmpdir, 'random', str(i))
+        )
+        for i in range(2)
     )
 
-    average_tpe_scores = (sum(tpe_scores) / len(tpe_scores))
-    average_random_scores = (sum(random_scores) / len(random_scores))
-    assert average_tpe_scores < average_random_scores
+    mean_tpe_score = np.array(tpe_scores).flatten().mean(axis=-1)
+    mean_random_score = np.array(random_scores).flatten().mean(axis=-1)
+
+    assert mean_tpe_score < mean_random_score
 
 
 
-def _test_tpe_score(expected_output_mult, pipeline, tmpdir):
-    hp_repository = InMemoryHyperparamsRepository(cache_folder=str(tmpdir))
+def _test_trial_scores(
+    expected_output_mult,
+    pipeline,
+    hyperparams_optimizer: BaseHyperparameterSelectionStrategy,
+    tmpdir: str
+):
+    hp_repository: InMemoryHyperparamsRepository = InMemoryHyperparamsRepository(cache_folder=str(tmpdir))
     n_epochs = 1
     n_trials = 20
-    auto_ml = AutoML(
+    auto_ml: AutoML = AutoML(
         pipeline=pipeline,
-        hyperparams_optimizer=TreeParzenEstimatorHyperparameterSelectionStrategy(
-            number_of_initial_random_step=10,
-            quantile_threshold=0.3,
-            number_good_trials_max_cap=25,
-            number_possible_hyperparams_candidates=100,
-            prior_weight=0.,
-            use_linear_forgetting_weights=False,
-            number_recent_trial_at_full_weights=25
-        ),
+        hyperparams_optimizer=hyperparams_optimizer,
         validation_splitter=ValidationSplitter(0.5),
         scoring_callback=ScoringCallback(mean_squared_error, higher_score_is_better=False),
         callbacks=[
@@ -113,37 +130,14 @@ def _test_tpe_score(expected_output_mult, pipeline, tmpdir):
         epochs=n_epochs,
         hyperparams_repository=hp_repository
     )
+
     # When
     data_inputs = np.array([0, 0])
     expected_outputs = expected_output_mult * np.ones_like(data_inputs)
-    auto_ml = auto_ml.fit(data_inputs=data_inputs, expected_outputs=expected_outputs)
+    auto_ml.fit(data_inputs=data_inputs, expected_outputs=expected_outputs)
+
     # Then
-    p = auto_ml.get_best_model().get_hyperparams()
+    trials: Trials = hp_repository.load_all_trials(status=TRIAL_STATUS.SUCCESS)
+    validation_scores = [t.get_validation_score() for t in trials]
+    return validation_scores
 
-    return abs(p["AddN__add"] - expected_output_mult)
-
-def _test_random_score(expected_output_mult, pipeline, tmpdir):
-    hp_repository = InMemoryHyperparamsRepository(cache_folder=str(tmpdir))
-    n_epochs = 1
-    n_trials = 20
-    auto_ml = AutoML(
-        pipeline=pipeline,
-        hyperparams_optimizer=RandomSearchHyperparameterSelectionStrategy(),
-        validation_splitter=ValidationSplitter(0.5),
-        scoring_callback=ScoringCallback(mean_squared_error, higher_score_is_better=False),
-        callbacks=[
-            MetricCallback('mse', metric_function=mean_squared_error, higher_score_is_better=False),
-        ],
-        n_trials=n_trials,
-        refit_trial=True,
-        epochs=n_epochs,
-        hyperparams_repository=hp_repository
-    )
-    # When
-    data_inputs = np.array([0, 0])
-    expected_outputs = expected_output_mult * np.ones_like(data_inputs)
-    auto_ml = auto_ml.fit(data_inputs=data_inputs, expected_outputs=expected_outputs)
-    # Then
-    p = auto_ml.get_best_model().get_hyperparams()
-
-    return abs(p["AddN__add"] - expected_output_mult)
