f330aa80a6b0e96fba185e3e28107315fd653b16
Vincent Antaki <mail@vincentantaki.com>
2020-11-16 11:09:12 -0500

    replaced calls to python's random module by call to numpy random.

--- neuraxle/hyperparams/distributions.py
+++ neuraxle/hyperparams/distributions.py
@@ -28,7 +28,6 @@ the distribution.
 
 import copy
 import math
-import random
 import sys
 from abc import abstractmethod, ABCMeta
 from typing import List, Tuple, Union, Any, Optional
@@ -803,7 +802,7 @@ class RandInt(DiscreteHyperparameterDistribution):
 
         :return: an integer.
         """
-        return random.randint(self.min_included, self.max_included)
+        return np.random.randint(self.min_included, self.max_included)
 
     def pdf(self, x) -> float:
         """
@@ -894,7 +893,7 @@ class Uniform(HyperparameterDistribution):
 
         :return: a float.
         """
-        return random.random() * (self.max_included - self.min_included) + self.min_included
+        return np.random.uniform() * (self.max_included - self.min_included) + self.min_included
 
     def pdf(self, x):
         """
