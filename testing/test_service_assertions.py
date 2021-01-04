9a015209250feabf80c5b18bb036195765f0997e
Vincent Antaki <mail@vincentantaki.com>
2021-01-04 11:07:59 -0500

    update test_service_assertion with change from last commit

--- testing/test_service_assertions.py
+++ testing/test_service_assertions.py
@@ -139,7 +139,7 @@ def _make_autoML_loop(tmpdir, p: Pipeline):
         epochs=n_epochs,
         hyperparams_repository=hp_repository,
         cache_folder_when_no_handle=str(tmpdir),
-        continue_loop_on_error=True
+        continue_loop_on_error=False
     )
 
 
