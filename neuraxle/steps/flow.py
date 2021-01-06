80b55aac0ec2e4f2b2e10feb50494431272189c1
Vincent Antaki <mail@vincentantaki.com>
2021-01-06 14:57:39 -0500

    Fixed a class attribute call I forgot to change in ChooseOneStepOf

--- neuraxle/steps/flow.py
+++ neuraxle/steps/flow.py
@@ -376,7 +376,7 @@ class ChooseOneStepOf(FeatureUnion):
 
     def _update_optional_hyperparams(self):
         step_names = list(self.keys())
-        chosen_step_name = self.hyperparams[CHOICE_HYPERPARAM] if CHOICE_HYPERPARAM in self.hyperparams else step_names[
+        chosen_step_name = self.hyperparams[self.CHOICE_HYPERPARAM] if self.CHOICE_HYPERPARAM in self.hyperparams else step_names[
             0]
 
         if chosen_step_name not in step_names:
