43a69ad5cde416332dc8315e332de5fc1a332ec8
Guillaume Chevalier <guillaume.c@kimoby.com>
2021-10-14 09:26:22 -0400

    Update workflows to python 3.9 and add quick test runners.

---
+++ run_quick_tests.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+py.test  testing/ --ignore=testing/metaopt/test_tpe.py --ignore=testing/examples/test_examples.py
---
+++ run_slow_tests.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+py.test  testing/metaopt/test_tpe.py testing/examples/test_examples.py
