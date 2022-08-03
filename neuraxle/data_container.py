464578aa7048dcd0a7328cf19d2dc34f1eaacc46
guillaume-chevalier <guillaume.c@kimoby.com>
2022-08-02 20:26:41 -0400

    Amend last commit

--- neuraxle/data_container.py
+++ neuraxle/data_container.py
@@ -500,12 +500,15 @@ class DataContainer(Generic[IDT, DIT, EOT]):
         :return: iterator of tuples containing ids, data_inputs, and expected outputs
         :rtype: Iterator[Tuple]
         """
+        if self.data_inputs is None:
+            return iter()
+
         _ids: Optional[List[DACTData]] = self.ids
         _di: Optional[List[DACTData]] = self.di
         _eo: Optional[List[DACTData]] = self.eo
-        if None in (_ids, _di, _eo):
-            # return empty iterator if one of the three is None
+        if _ids is None or _di is None or _eo is None:
             return iter(())
+
         return zip(_ids, _di, _eo)
 
     def __repr__(self):
